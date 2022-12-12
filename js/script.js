5ed13a73e47b7ea72c26c20e06e64f788fa739c5
KnusperInge <111782820+KnusperInge@users.noreply.github.com>
2022-12-12 13:46:34 +0100

    added Temps, Navbar, Header, sort Css

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    includeHtml();
+})
+
+
+async function includeHtml() {
+    let includeElem = document.querySelectorAll('[include-html]');
+    for (let i = 0; i < includeElem.length; i++) {
+        const element = includeElem[i];
+        file = element.getAttribute("include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Element not Found';
+        }
+    }
+}
