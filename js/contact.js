09043d08194a77eda763a2db9eb103ff138e0d2b
Daniel <81906554+danield208@users.noreply.github.com>
2022-12-14 22:09:52 +0100

    contact update

--- js/contact.js
+++ js/contact.js
@@ -8,40 +8,104 @@ async function loadContactList() {
 }
 
 function loadLetters() {
+    loadLettersArray()
+    letters.sort();
+    generateLetterList()
+}
+
+function loadLettersArray() {
     document.getElementById('contact-list').innerHTML = '';
     contactArray.forEach(element => {
-        let letter = element['surname'].charAt(0).toUpperCase();
-        if (letters.includes(letter) == false) {
+        let letter = element.surname.charAt(0).toUpperCase();
+        if (!includesLetter(letter)) {
             letters.push(letter);
         }
     });
+}
 
-    letters.sort();
-
+function generateLetterList() {
     letters.forEach(letter => {
         letterSmall = letter.toLowerCase()
-        document.getElementById('contact-list').innerHTML += `
-            <div class="contact-lettercompartment">
-                <span>${letter}</span>
-                <hr>
-                <div id="${letterSmall}-list">
+        document.getElementById('contact-list').innerHTML += generateLetterHTML(letter, letterSmall);
+        loadContacts(letter)
+    });
+}
+
+function loadContacts(letter) {
+    contactArray.forEach(contact => {
+        if (getSurChar(contact) == letter) {
+            document.getElementById(`${letterSmall}-list`).innerHTML += generateContactListHTML(contact);
+        }
+    });
+}
+
+function openContactInfo(currentMail) {
+    let contact = contactArray.find(contactArray => contactArray.email === currentMail)
+    document.getElementById('contact-informations').innerHTML = generateContactInfoHTML(contact);
+}
+
+
+
+// ANCHOR IF questions
+function includesLetter(letter) {
+   return letters.includes(letter)
+}
+
+function getSurChar(contact) {
+   return  contact.surname.charAt(0)
+}
+
+
+// ANCHOR HTML include
+function generateLetterHTML(letter, letterSmall) {
+    return `
+        <div class="contact-lettercompartment">
+            <span>${letter}</span>
+            <hr>
+            <div id="${letterSmall}-list">
+            </div>
+        </div>
+    `;
+}
+
+function generateContactListHTML(contact) {
+    return `
+        <div class="listed-contact" onclick="openContactInfo('${contact.email}')">
+            <img src="./img/info.png">
+            <div class="listed-contact-info">
+                <span>${contact.name} ${contact.surname}</span>
+                <span class="contact-list-mail">${contact.email}</span>
+            </div>
+        </div>
+    `;
+}
+
+function generateContactInfoHTML(contact) {
+    return `
+        <div class="contact-header">
+            <img src="${contact.icon}" class="contact-img">
+            <div>
+                <span>${contact.name} ${contact.surname}</span>
+                <div class="addtask-box">
+                    <img src="./img/plus_Icon.png">
+                    <span>Add Task</span>
                 </div>
             </div>
-        `;
-
-        let list = document.getElementById(`${letterSmall}-list`);
-        contactArray.forEach(contact => {
-            if(contact['surname'].charAt(0) == letter) {
-                list.innerHTML += `
-                    <div class="listed-contact">
-                        <img src="./img/info.png">
-                        <div class="listed-contact-info">
-                            <span>${contact['name']} ${contact['surname']}</span>
-                            <span class="contact-mail">${contact['email']}</span>
-                        </div>
-                    </div>
-                `;
-            }
-        });
-    });
+        </div>
+        <div class="contact-mid">
+            <span>Contact Information</span>
+            <div>
+                <img src="./img/pencil.png">
+                <span>Edit Contact</span>
+            </div>
+        </div>
+        <div class="contact-mail">
+            <span>E-Mail</span>
+            <span>${contact.email}</span>
+        </div>
+        <div class="contact-number">
+            <span>Mobil</span>
+            <span>${contact.number[1].mobile}</span>
+        </div>
+    `;
 }
