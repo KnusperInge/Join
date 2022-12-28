070486a1dea9537f2a0b199ab8cbd1eede5b43ca
Daniel <81906554+danield208@users.noreply.github.com>
2022-12-28 15:09:01 +0100

    small changes

--- js/loadTemplates.js
+++ js/loadTemplates.js
@@ -1,43 +1,39 @@
-
 document.addEventListener("DOMContentLoaded", async () => {
-  await includeHtml();
+	await includeHtml();
 
-  navItems = checkNavItem();
-  page = checkItems();
-  setActivelink();
-
-
-})
-
+	navItems = checkNavItem();
+	page = checkItems();
+	setActivelink();
+});
 
 async function includeHtml() {
-  let includeElem = document.querySelectorAll('[include-html]');
-  for (let i = 0; i < includeElem.length; i++) {
-    const element = includeElem[i];
-    file = element.getAttribute("include-html");
-    let resp = await fetch(file);
-    if (resp.ok) {
-      element.innerHTML = await resp.text();
-    } else {
-      element.innerHTML = 'Element not Found';
-    }
-  }
+	let includeElem = document.querySelectorAll("[include-html]");
+	for (let i = 0; i < includeElem.length; i++) {
+		const element = includeElem[i];
+		file = element.getAttribute("include-html");
+		let resp = await fetch(file);
+		if (resp.ok) {
+			element.innerHTML = await resp.text();
+		} else {
+			element.innerHTML = "Element not Found";
+		}
+	}
 }
 
 function checkNavItem() {
-  return document.querySelectorAll("nav ul li");
+	return document.querySelectorAll("nav ul li");
 }
 
 function checkItems() {
-  let item = document.querySelector('[item]');
-  let element = item.getAttribute("item");
-  return element;
+	let item = document.querySelector("[item]");
+	let element = item.getAttribute("item");
+	return element;
 }
 
 function setActivelink() {
-  navItems.forEach((item) => {
-    if (item.textContent == page) {
-      item.classList.add('activeNavItem');
-    }
-  })
+	navItems.forEach((item) => {
+		if (item.textContent == page) {
+			item.classList.add("activeNavItem");
+		}
+	});
 }
