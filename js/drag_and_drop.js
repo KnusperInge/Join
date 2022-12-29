01fdab2b791bf99250c29650656409f27ffd4401
Daniel <81906554+danield208@users.noreply.github.com>
2022-12-29 13:38:04 +0100

    first try drag and drop

--- js/drag_and_drop.js
+++ js/drag_and_drop.js
@@ -1,5 +1,8 @@
+let currentDragElement;
+
 function startDragging(title) {
 	currentDragElement = title;
+	console.log("startDragging:" + currentDragElement);
 }
 
 function allowDrop(event) {
@@ -7,12 +10,22 @@ function allowDrop(event) {
 }
 
 function drop(category) {
-	todos[currentDragElement].Status = category;
+	let index = Tasks.findIndex((element) => element.Title == currentDragElement);
+	console.log(index);
+	Tasks[index].Status = category;
+	loadTasks();
 }
 
 // ONLY FOR TESTING
 document.addEventListener("DOMContentLoaded", () => {
+	loadTasks();
+});
+
+function loadTasks() {
 	document.querySelector("#toDo").innerHTML = "";
+	document.querySelector("#inprogress").innerHTML = "";
+	document.querySelector("#feedback").innerHTML = "";
+	document.querySelector("#done").innerHTML = "";
 	const template = document.querySelector("#task_card");
 	const clonedTemp = template.content.cloneNode(true);
 
@@ -20,11 +33,11 @@ document.addEventListener("DOMContentLoaded", () => {
 		const currentTask = Tasks[i];
 		const todoList = document.querySelector("#" + currentTask.Status);
 		let tempContent = clonedTemp.querySelectorAll("div, h4, span, img");
-		tempContent[0].attributes[2].nodeValue = `startDragging(${currentTask.Title})`;
+		tempContent[0].attributes[2].nodeValue = `startDragging("${currentTask.Title}")`;
 		tempContent[1].innerHTML = currentTask.Category;
 		tempContent[2].innerHTML = currentTask.Title;
 		tempContent[4].innerHTML = currentTask.Description;
 		console.log(tempContent);
 		todoList.appendChild(clonedTemp);
 	}
-});
+}
