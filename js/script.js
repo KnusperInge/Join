2a77b5e267fc99e21abc097cdf3f8e55c5909e01
Alex-Zoltan Tofan <117070956+AlexZoltanTofan@users.noreply.github.com>
2023-01-03 21:11:00 +0100

    Contacts-random-color

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-setURL("https://gruppe-398.developerakademie.net/smallest_backend_ever");
+setURL('https://gruppe-398.developerakademie.net/smallest_backend_ever');
 
 let Tasks = [];
 let navItems = [];
 let contactList = [];
-let page = "";
-let selectedContacts = document.querySelector(".selectedContacts");
+let page = '';
+let selectedContacts = document.querySelector('.selectedContacts');
 let newTask = new Task();
 let loadingPrps;
 let currentDragElement;
@@ -12,147 +12,148 @@ let currentDragElement;
 //general functions
 
 function openboard() {
-	window.open((href = "./board.html"), "_self");
+  window.open((href = './board.html'), '_self');
 }
 
 // open/close task- Sidemenu
-if (window.location.pathname == "/contact.html") {
-	document.querySelector("#addTask_button").addEventListener("click", (event) => {
-		document.querySelector(".newTask").classList.remove("d-none");
-		console.log(event);
-	});
-	document.querySelector(".close-icon").addEventListener("click", (event) => {
-		document.querySelector(".newTask").classList.add("d-none");
-		console.log(event);
-	});
+if (window.location.pathname == '/contact.html') {
+  document
+    .querySelector('#addTask_button')
+    .addEventListener('click', (event) => {
+      document.querySelector('.newTask').classList.remove('d-none');
+      console.log(event);
+    });
+  document.querySelector('.close-icon').addEventListener('click', (event) => {
+    document.querySelector('.newTask').classList.add('d-none');
+    console.log(event);
+  });
 }
 
 //Loading Element
-document.addEventListener("DOMContentLoaded", testinit);
+document.addEventListener('DOMContentLoaded', testinit);
 
 async function testinit() {
-	await downloadFromServer();
-	//Load from server
-	loadData();
-	// currentDragElement = new DragandDrop();
-	// currentDragElement.loadTasks();
-	// console.log("Tasks Array:", Tasks);
-	let item = document.querySelector("[item]").getAttribute("item");
-	if (item === "AddTask" || item === "Board") {
-		console.log('Nicht gefunden ', item);
-	} else {
-		loadingPrps = new Loading();
-
-	}
+  await downloadFromServer();
+  //Load from server
+  loadData();
+  // currentDragElement = new DragandDrop();
+  // currentDragElement.loadTasks();
+  // console.log("Tasks Array:", Tasks);
+  let item = document.querySelector('[item]').getAttribute('item');
+  if (item === 'AddTask' || item === 'Board') {
+    console.log('Nicht gefunden ', item);
+  } else {
+    loadingPrps = new Loading();
+  }
 }
 
 //Save and Load
 function saveData() {
-	backend.setItem("Tasks", JSON.stringify(Tasks));
+  backend.setItem('Tasks', JSON.stringify(Tasks));
 }
 
 async function loadData() {
-	Tasks = JSON.parse(backend.getItem("Tasks")) || [];
+  Tasks = JSON.parse(backend.getItem('Tasks')) || [];
 }
 
 //Task.html functions
 function checkInput(field) {
-	let input = document.querySelector(`.${field}`);
-	if (input.value) {
-		input.classList.add("blackTextColor");
-	} else {
-		input.classList.remove("blackTextColor");
-	}
+  let input = document.querySelector(`.${field}`);
+  if (input.value) {
+    input.classList.add('blackTextColor');
+  } else {
+    input.classList.remove('blackTextColor');
+  }
 }
 function changePriority(str) {
-	if (str == 1) {
-		newTask.aktivateUrgent();
-	} else if (str == 2) {
-		newTask.aktivateMedium();
-	} else {
-		newTask.aktivateLow();
-	}
+  if (str == 1) {
+    newTask.aktivateUrgent();
+  } else if (str == 2) {
+    newTask.aktivateMedium();
+  } else {
+    newTask.aktivateLow();
+  }
 }
 
 // Script for Dropdown Menus
 function openList(id) {
-	let dropdowns = document.querySelectorAll(".dropdown .list");
-	if (id == 3) {
-		false;
-	} else {
-		dropdowns[id].classList.toggle("active");
-	}
+  let dropdowns = document.querySelectorAll('.dropdown .list');
+  if (id == 3) {
+    false;
+  } else {
+    dropdowns[id].classList.toggle('active');
+  }
 
-	if (!document.querySelector(".dropdown .active") && contactList.length > 0) {
-		renderIcons();
-		selectedContacts.classList.toggle("active");
-	} else {
-		selectedContacts.classList.remove("active");
-		selectedContacts.innerHTML = "";
-	}
+  if (!document.querySelector('.dropdown .active') && contactList.length > 0) {
+    renderIcons();
+    selectedContacts.classList.toggle('active');
+  } else {
+    selectedContacts.classList.remove('active');
+    selectedContacts.innerHTML = '';
+  }
 }
 
 function addContact(id) {
-	let selectableObject = document.querySelectorAll(".list div");
-	let checkedChild = selectableObject[id].lastElementChild;
-	let contact = selectableObject[id].innerText;
-	if (!checkedChild.checked && !checkArr(contact)) {
-		checkedChild.checked = true;
-		contactList.push(contact);
-	} else {
-		contactList.splice(contactList.indexOf(contact), 1);
-		checkedChild.checked = false;
-	}
+  let selectableObject = document.querySelectorAll('.list div');
+  let checkedChild = selectableObject[id].lastElementChild;
+  let contact = selectableObject[id].innerText;
+  if (!checkedChild.checked && !checkArr(contact)) {
+    checkedChild.checked = true;
+    contactList.push(contact);
+  } else {
+    contactList.splice(contactList.indexOf(contact), 1);
+    checkedChild.checked = false;
+  }
 }
 
 function checkArr(contact) {
-	return contactList.includes(contact);
+  return contactList.includes(contact);
 }
 
 function renderIcons() {
-	contactList.forEach((element) => {
-		let firstletter = element.charAt(0).toUpperCase();
-		selectedContacts.innerHTML += `<span>${firstletter}</span>`;
-	});
+  contactList.forEach((element) => {
+    let firstletter = element.charAt(0).toUpperCase();
+    selectedContacts.innerHTML += `<span>${firstletter}</span>`;
+  });
 }
 
 function inviteContact() {
-	document.getElementById("searchContacts").classList.remove("d-none");
-	document.getElementById("dropdownContacts").classList.add("d-none");
-	openList();
+  document.getElementById('searchContacts').classList.remove('d-none');
+  document.getElementById('dropdownContacts').classList.add('d-none');
+  openList();
 }
 
 // handle Task Forms
-if (window.location.pathname == "/task.html") {
-	document.getElementById("TaskForm").addEventListener("submit", handleForm);
+if (window.location.pathname == '/task.html') {
+  document.getElementById('TaskForm').addEventListener('submit', handleForm);
 
-	document.querySelector("#clear-btn").addEventListener("click", (event) => {
-		event.preventDefault();
-		newTask.clearForm();
-	});
+  document.querySelector('#clear-btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    newTask.clearForm();
+  });
 }
 
 function handleForm(event) {
-	event.preventDefault();
-	newTask.init();
-	Tasks.push(newTask);
-	saveData();
-	newTask.clearForm();
+  event.preventDefault();
+  newTask.init();
+  Tasks.push(newTask);
+  saveData();
+  newTask.clearForm();
 }
 
 function addCategory(id) {
-	let input = document.querySelector(".cagetorgy-input");
-	//input.innerHTML="";
-	let Category = checkCategory(id);
-	input.value = Category;
-	if (!input.value == "") {
-		newTask.Category = Category;
-	}
+  let input = document.querySelector('.cagetorgy-input');
+  //input.innerHTML="";
+  let Category = checkCategory(id);
+  input.value = Category;
+  if (!input.value == '') {
+    newTask.Category = Category;
+  }
 }
 
 function checkCategory(id) {
-	let catlist = document.querySelectorAll(".category div");
-	return catlist[id].textContent;
+  let catlist = document.querySelectorAll('.category div');
+  return catlist[id].textContent;
 }
 
 // board.html
