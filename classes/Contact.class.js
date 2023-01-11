870a31299b229640c5548df0b388b21effa436f0
KnusperInge <111782820+KnusperInge@users.noreply.github.com>
2023-01-11 15:56:05 +0100

    Update Contact.class.js

--- classes/Contact.class.js
+++ classes/Contact.class.js
@@ -14,42 +14,40 @@ class Contact {
 	letters = [];
 
 	constructor() {
-		//this.initContactClass();
 		this.loadContactList();
-		this.addEventListener();
+		this.setButtons();
+
 
 	}
-
 
 	loadContactList() {
-		this.loadLettersArray();
+		let target = document.getElementById("contact-list");
+		target.innerHTML = "";
+		this.initLettersArr();
+		this.generateLetterList(target);
 	}
 
-	loadLettersArray() {
-		document.getElementById("contact-list").innerHTML = "";
+	initLettersArr() {
 		contactList.forEach((element) => {
-			let letter = element.Surname.charAt(0).toUpperCase();
+			let letter = element.Surname.charAt(0);
 			if (!this.includesLetter(letter)) {
 				this.letters.push(letter);
 			}
-
 		});
 		this.letters.sort();
-		this.generateLetterList();
-		// random_bg_color();
 	}
 
 	includesLetter(letter) {
 		return this.letters.includes(letter);
 	}
 
-	generateLetterList() {
+	generateLetterList(target) {
 		this.letters.forEach((letter) => {
 			const letterTemp = document.getElementById("letter_list_template").content.cloneNode(true);
 			const tempContent = letterTemp.querySelectorAll("div, span");
 			tempContent[1].innerHTML = letter;
 			tempContent[2].id = letter + "-list";
-			document.getElementById("contact-list").appendChild(letterTemp);
+			target.appendChild(letterTemp);
 			this.loadContacts(letter);
 		});
 	}
@@ -58,6 +56,9 @@ class Contact {
 		contactList.forEach((contact) => {
 			const contactListTemp = document.getElementById("contact_inList_template").content.cloneNode(true);
 			const tempContent = contactListTemp.querySelectorAll("div, img, span");
+			console.log(tempContent);
+
+			// Das besser auschreiben
 			tempContent[0].id = contact.ID;
 			tempContent[3].innerHTML = contact.Name + " " + contact.Surname;
 			tempContent[4].innerHTML = contact.Mail;
@@ -70,6 +71,110 @@ class Contact {
 	getSurChar(contact) {
 		return contact.Surname.charAt(0);
 	}
+	// add event listener
+	setButtons() {
+		this.closeNewTask();
+		this.openContactInfo();
+		this.closeContactInfo();
+		this.addNewContact();
+	}
+	closeNewTask() {
+		document.querySelector(".close-icon").addEventListener("click", () => {
+			document.querySelector(".newTask").classList.toggle("open");
+			document.getElementById("overlay").classList.toggle("open");
+			document.querySelector(".AddButton").classList.toggle("open");
+		});
+	}
+
+	openContactInfo() {
+		this.loadContactIds();
+		this.ContactInfoEventListener();
+	}
+
+	loadContactIds() {
+		contactList.forEach((contact) => {
+			this.contactIDs.push(contact.ID);
+		});
+	}
+
+	// ANCHOR open contact info
+	ContactInfoEventListener() {
+		this.contactIDs.forEach((id) => {
+			document.getElementById(`${id}`).addEventListener("click", () => {
+				// ToDo auslagern
+				document.querySelector(".contact-right-container").classList.toggle("active");
+				document.querySelector(".close_contact_info_tablet").classList.toggle("active");
+				document.getElementById("contact-informations").classList.toggle("active");
+				const contactInfoTemp = document.getElementById("contact_info_template").content.cloneNode(true);
+				const tempContent = contactInfoTemp.querySelectorAll("div, img, span");
+				const contact = contactList.find((contact) => contact.id == id);
+				tempContent[3].innerHTML = contact.name + " " + contact.surname;
+				tempContent[14].innerHTML = contact.email;
+				tempContent[17].innerHTML = contact.phone;
+				document.getElementById("contact-informations").innerHTML = "";
+				document.getElementById("contact-informations").append(contactInfoTemp);
+				document.querySelector(".edidContact").id = "edidContact" + id;
+				// getInfosToAddNewTask();
+				this.edidContact();
+				this.openNewTask();
+				this.closeNewTask();
+			});
+		});
+	}
+
+	closeContactInfo() {
+		document.querySelector(".close_contact_info_tablet").addEventListener("click", () => {
+			document.querySelector(".contact-right-container").classList.toggle("active");
+			document.querySelector(".close_contact_info_tablet").classList.toggle("active");
+			document.getElementById("contact-informations").classList.toggle("active");
+			document.getElementById("contact-informations").innerHTML = "";
+		});
+	}
+
+	// ANCHOR edid contact
+	edidContact() { }
+
+	// ANCHOR new task in contact info
+	openNewTask() {
+		document.querySelector(".addtask-box").addEventListener("click", () => {
+			newTask = new Task();
+			document.querySelector(".newTask").classList.toggle("open");
+			document.getElementById("overlay").classList.toggle("open");
+			document.querySelector(".AddButton").classList.toggle("open");
+		});
+	}
+
+	// ANCHOR new task
+
+
+	// ANCHOR new contact
+	addNewContact() {
+		document.querySelector(".add-contact").addEventListener("click", () => {
+			document.getElementById("new-contact").classList.toggle("open");
+			document.getElementById("overlay").classList.toggle("open");
+			setTimeout(() => {
+				document.getElementById("new-contact-content").classList.toggle("open");
+			}, 300);
+		});
+		this.closeNewContact();
+	}
+
+	closeNewContact() {
+		let cancelAdd = document.querySelectorAll(".cancel_add");
+		cancelAdd[0].addEventListener("click", () => {
+			document.getElementById("new-contact").classList.toggle("open");
+			document.getElementById("overlay").classList.toggle("open");
+			document.getElementById("new-contact-content").classList.toggle("open");
+		});
+
+		cancelAdd[1].addEventListener("click", () => {
+			document.getElementById("new-contact").classList.toggle("open");
+			document.getElementById("overlay").classList.toggle("open");
+			document.getElementById("new-contact-content").classList.toggle("open");
+		});
+	}
+
+
 
 	// Create new Contact
 	createContact() {
@@ -118,106 +223,7 @@ class Contact {
 			BgColor: this.bgcolor,
 		});
 		saveData();
+		loadData();
 	}
 
-	// add event listener
-	addEventListener() {
-		this.closeNewTask();
-		this.openContactInfo();
-		this.closeContactInfo();
-		this.addNewContact();
-	}
-
-	openContactInfo() {
-		this.loadContactIds();
-		this.ContactInfoEventListener();
-	}
-
-	loadContactIds() {
-		contactList.forEach((contact) => {
-			this.contactIDs.push(contact.ID);
-		});
-	}
-
-	// ANCHOR open contact info
-	ContactInfoEventListener() {
-		this.contactIDs.forEach((id) => {
-			document.getElementById(`${id}`).addEventListener("click", () => {
-				document.querySelector(".contact-right-container").classList.toggle("active");
-				document.querySelector(".close_contact_info_tablet").classList.toggle("active");
-				document.getElementById("contact-informations").classList.toggle("active");
-				const contactInfoTemp = document.getElementById("contact_info_template").content.cloneNode(true);
-				const tempContent = contactInfoTemp.querySelectorAll("div, img, span");
-				const contact = contactList.find((contact) => contact.id == id);
-				tempContent[3].innerHTML = contact.name + " " + contact.surname;
-				tempContent[14].innerHTML = contact.email;
-				tempContent[17].innerHTML = contact.phone;
-				document.getElementById("contact-informations").innerHTML = "";
-				document.getElementById("contact-informations").append(contactInfoTemp);
-				document.querySelector(".edidContact").id = "edidContact" + id;
-				// getInfosToAddNewTask();
-				this.edidContact();
-				this.openNewTask();
-				this.closeNewTask();
-			});
-		});
-	}
-
-	closeContactInfo() {
-		document.querySelector(".close_contact_info_tablet").addEventListener("click", () => {
-			document.querySelector(".contact-right-container").classList.toggle("active");
-			document.querySelector(".close_contact_info_tablet").classList.toggle("active");
-			document.getElementById("contact-informations").classList.toggle("active");
-			document.getElementById("contact-informations").innerHTML = "";
-		});
-	}
-
-	// ANCHOR edid contact
-	edidContact() { }
-
-	// ANCHOR new task in contact info
-	openNewTask() {
-		document.querySelector(".addtask-box").addEventListener("click", () => {
-			newTask = new Task();
-			document.querySelector(".newTask").classList.toggle("open");
-			document.getElementById("overlay").classList.toggle("open");
-			document.querySelector(".AddButton").classList.toggle("open");
-		});
-	}
-
-	// ANCHOR new task
-	closeNewTask() {
-		document.querySelector(".close-icon").addEventListener("click", () => {
-			document.querySelector(".newTask").classList.toggle("open");
-			document.getElementById("overlay").classList.toggle("open");
-			document.querySelector(".AddButton").classList.toggle("open");
-		});
-	}
-
-	// ANCHOR new contact
-	addNewContact() {
-		document.querySelector(".add-contact").addEventListener("click", () => {
-			document.getElementById("new-contact").classList.toggle("open");
-			document.getElementById("overlay").classList.toggle("open");
-			setTimeout(() => {
-				document.getElementById("new-contact-content").classList.toggle("open");
-			}, 300);
-		});
-		this.closeNewContact();
-	}
-
-	closeNewContact() {
-		let cancelAdd = document.querySelectorAll(".cancel_add");
-		cancelAdd[0].addEventListener("click", () => {
-			document.getElementById("new-contact").classList.toggle("open");
-			document.getElementById("overlay").classList.toggle("open");
-			document.getElementById("new-contact-content").classList.toggle("open");
-		});
-
-		cancelAdd[1].addEventListener("click", () => {
-			document.getElementById("new-contact").classList.toggle("open");
-			document.getElementById("overlay").classList.toggle("open");
-			document.getElementById("new-contact-content").classList.toggle("open");
-		});
-	}
 }
