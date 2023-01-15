792f202315960a1a77a4febf5ab170790d95e60e
Alex-Zoltan Tofan <117070956+AlexZoltanTofan@users.noreply.github.com>
2023-01-15 12:21:14 +0100

    format change

--- classes/loadProperties.class.js
+++ classes/loadProperties.class.js
@@ -6,46 +6,63 @@ class Loading {
   awaitArr = [];
   doneTaskArr = [];
   dates = [];
-  MONTHS = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"];
-  dayPeriod = ["Good morning, ", "Hello, ", "Good evening, "];
+  MONTHS = [
+    'Januar',
+    'Februar',
+    'März',
+    'April',
+    'Mai',
+    'Juni',
+    'Juli',
+    'August',
+    'September',
+    'Oktober',
+    'November',
+    'Dezember',
+  ];
+  dayPeriod = ['Good morning, ', 'Hello, ', 'Good evening, '];
   YEAR = 0;
-  MONTH = "";
+  MONTH = '';
   DAY = 0;
-  FinalDate = "";
+  FinalDate = '';
   constructor() {
-
     this.allTasksArr = Tasks;
     this.checkUrgent();
     this.checkStatus();
     this.init();
-
   }
   checkStatus() {
     this.allTasksArr.forEach((arr) => {
-      if (arr.Status == "toDo") {
+      if (arr.Status == 'toDo') {
         this.toDoTaskArr.push(arr);
-
-      } if (arr.Status == "inProgress") {
-        this.inProgressArr.push(arr);
-
-      } if (arr.Status == "Await") {
-        this.awaitArr.push(arr);
-        document.getElementById('toDoTasks').innerText = this.toDoTaskArr.length;
-      } if (this.Status == "Done") {
-        this.doneTaskArr.push(arr);
-        document.getElementById('toDoTasks').innerText = this.toDoTaskArr.length;
       }
-    })
+      if (arr.Status == 'inProgress') {
+        this.inProgressArr.push(arr);
+      }
+      if (arr.Status == 'Await') {
+        this.awaitArr.push(arr);
+        document.getElementById('toDoTasks').innerText =
+          this.toDoTaskArr.length;
+      }
+      if (this.Status == 'Done') {
+        this.doneTaskArr.push(arr);
+        document.getElementById('toDoTasks').innerText =
+          this.toDoTaskArr.length;
+      }
+    });
   }
 
   checkUrgent() {
     //Check Main Arr for urgent
-    this.allTasksArr.forEach(arr => {
-      if (arr.Priority == "Urgent" && arr.Status == "toDo" || arr.Status == "inProgress") {
+    this.allTasksArr.forEach((arr) => {
+      if (
+        (arr.Priority == 'Urgent' && arr.Status == 'toDo') ||
+        arr.Status == 'inProgress'
+      ) {
         this.urgentTasksArr.push(arr);
       }
     });
-    this.urgentTasksArr.forEach(date => {
+    this.urgentTasksArr.forEach((date) => {
       this.dates.push(Date.parse(date.Deadline));
     });
     //Nummeric sort of the Dates
@@ -55,21 +72,24 @@ class Loading {
   }
 
   init() {
-    document.getElementById('urgentTasks').innerText = this.urgentTasksArr.length;
+    document.getElementById('urgentTasks').innerText =
+      this.urgentTasksArr.length;
     document.getElementById('upcomingDeadline').innerText = this.createDate();
     document.getElementById('AllTask').innerText = this.allTasksArr.length;
     document.getElementById('toDoTasks').innerText = this.toDoTaskArr.length;
     document.getElementById('inProgress').innerText = this.inProgressArr.length;
-    document.getElementById('awaitForFeedback').innerText = this.awaitArr.length;
+    document.getElementById('awaitForFeedback').innerText =
+      this.awaitArr.length;
     document.getElementById('done').innerText = this.doneTaskArr.length;
-    document.querySelector(".first-line").childNodes[1].innerText = this.getTimestampInSeconds();
+    document.querySelector('.first-line').childNodes[1].innerText =
+      this.getTimestampInSeconds();
   }
 
   createDate() {
     this.YEAR = new Date(this.dates[0]).getFullYear();
     this.MONTH = this.MONTHS[new Date(this.dates[0]).getMonth()];
     this.DAY = new Date(this.dates[0]).getDate();
-    return this.FinalDate = `${this.MONTH} ${this.DAY}, ${this.YEAR}`;
+    return (this.FinalDate = `${this.MONTH} ${this.DAY}, ${this.YEAR}`);
   }
 
   getTimestampInSeconds() {
@@ -84,6 +104,4 @@ class Loading {
       return this.dayPeriod[2];
     }
   }
-
-
 }
