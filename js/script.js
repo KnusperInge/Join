b1a9b0053a1c4e43cc26137dc716693c44536caa
Daniel <81906554+danield208@users.noreply.github.com>
2023-01-19 19:25:04 +0100

    format stuff

--- js/script.js
+++ js/script.js
@@ -1,25 +1,15 @@
 setURL("https://gruppe-398.developerakademie.net/smallest_backend_ever");
 
-let Tasks = [], navItems = [], contactList = [];
+let Tasks = [];
+let navItems = [];
+let contactList = [];
+let Categories = [];
+let lastChosenEditor = [];
 let page = "";
-let newTask, loadingPrps, currentDragElement;
-let Categories = [
-	{
-		name: "Sales",
-		id: 0,
-		Color: ""
-	},
-	{
-		name: "Development",
-		id: 1,
-		Color: ""
-	},
-	{
-		name: "Marketing",
-		id: 2,
-		Color: ""
-	}
-];
+
+let newTask;
+let loadingPrps;
+let currentDragElement;
 
 //general functions
 
@@ -48,7 +38,6 @@ function loadingClasses(item) {
 		});
 	} else if (item == "AddTask") {
 		newTask = new Task();
-		document.getElementById("TaskForm").addEventListener("submit", handleForm);
 	} else if (item == "Contacts") {
 		contact = new Contact();
 		document.getElementById("TaskForm").addEventListener("submit", handleForm);
@@ -76,17 +65,18 @@ function deleteShwonContact() {
 }
 
 //Save and Load
-function saveData() {
-	backend.setItem("Tasks", JSON.stringify(Tasks));
-	backend.setItem("Contacts", JSON.stringify(contactList));
-	backend.setItem("Categories", JSON.stringify(Categories));
-
+async function saveData() {
+	await backend.setItem("Tasks", JSON.stringify(Tasks));
+	await backend.setItem("Contacts", JSON.stringify(contactList));
+	await backend.setItem("Categories", JSON.stringify(Categories));
+	await backend.setItem("lastChosenEditor", JSON.stringify(lastChosenEditor));
 }
 
-function loadData() {
-	Tasks = (JSON.parse(backend.getItem("Tasks"))) || [];
-	contactList = (JSON.parse(backend.getItem("Contacts"))) || [];
-	Categories = (JSON.parse(backend.getItem("Categories"))) || [];
+async function loadData() {
+	Tasks = (await JSON.parse(backend.getItem("Tasks"))) || [];
+	contactList = (await JSON.parse(backend.getItem("Contacts"))) || [];
+	Categories = (await JSON.parse(backend.getItem("Categories"))) || [];
+	lastChosenEditor = (await JSON.parse(backend.getItem("lastChosenEditor"))) || [];
 }
 
 //Task.html functions
