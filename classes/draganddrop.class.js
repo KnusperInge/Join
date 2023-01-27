c68539e70f27cf1c539f5579acf89995e86ff346
Daniel <81906554+danield208@users.noreply.github.com>
2023-01-27 11:07:57 +0100

    update get task details

--- classes/draganddrop.class.js
+++ classes/draganddrop.class.js
@@ -14,7 +14,6 @@ class DragandDrop {
 		this.category = category;
 		this.Tasks = Tasks;
 		this.loadTasks();
-		this.setDetailBtn();
 		this.changeStatusInitEventListener();
 		this.initTaskFilter();
 	}
@@ -60,6 +59,18 @@ class DragandDrop {
 		this.template.querySelector(".bord-tasks-container-task h4").innerText = element.Title;
 		this.template.querySelector(".bord-task-desc span").innerText = element.Description;
 		this.template.querySelector(".bord-task-editor img").src = this.checkPriority(element);
+		this.template.querySelector(".bord-tasks-container-task").addEventListener("click", (event) => {
+			event.stopPropagation();
+			this.showTasskDetail(element.Title);
+		});
+	}
+
+	showTasskDetail(elemTitle) {
+		let Task = Tasks.find((task) => task.Title === elemTitle);
+		this.openDetail();
+		this.renderDetailHead(Task);
+		this.renderDetailBody(Task);
+		this.addDetailCloseBtn();
 	}
 
 	// ANCHOR add drag and drop event listener
@@ -101,17 +112,6 @@ class DragandDrop {
 		this.template.getElementById("progressBar-done").style.width = progressbarPercentage + "%";
 		this.template.getElementById("subTasks-inProgress").innerHTML = subTasksInProgress;
 		this.template.getElementById("subTasks-done").innerHTML = subTasksDone;
-	}
-
-	setDetailBtn() {
-		let allShortTasks = document.querySelectorAll(".bord-tasks-container-task");
-		allShortTasks.forEach((element) => {
-			element.addEventListener("click", (event) => {
-				this.openDetail(event);
-				this.loadDetailContent(event);
-			});
-		});
-		this.addDetailCloseBtn();
 	}
 
 	openDetail(event) {
