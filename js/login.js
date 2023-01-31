1762dc75d39d0cae8168d67a45986b0cf0bc01d7
Daniel <81906554+danield208@users.noreply.github.com>
2023-01-31 16:04:30 +0100

    login push

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
 addEventListener("animationend", () => {
 	document.getElementById("loading-page").style = "display: none;";
+	initLoginPage();
 });
+
+function initLoginPage() {
+	addEventListeners();
+}
+
+function addEventListeners() {
+	document.getElementById("loginButton").addEventListener("click", setLogin);
+	document.getElementById("signupButton").addEventListener("click");
+}
+
+function setLogin() {
+	document.getElementById("eMailInput").value = "joinLogin@join.de";
+	document.getElementById("passwordInput").value = "wasEinCoolesPassword";
+}
